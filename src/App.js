89df639ae5d693553a97bd26af7a81ff54a884bb
YSfallingStar <wjddbtjd55@naver.com>
2022-03-16 10:46:17 +0900

    Initial Upload

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import Left1 from './components/Left1';
+import Right1 from './components/Right1';
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h3>Root</h3>
+      <div id="container">
+        <Left1></Left1>
+        <Right1></Right1>
+      </div>
     </div>
   );
 }
